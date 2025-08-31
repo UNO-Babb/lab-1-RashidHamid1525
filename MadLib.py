2b59671310633b55c1bdec8829ff4ce77af01e37
RashidHamid1525 <rhamid@unomaha.edu>
2025-08-31 18:59:09 +0000

    Done with lab1

--- MadLib.py
+++ MadLib.py
@@ -6,8 +6,22 @@
 def main():
   print("Madlib")
   #Ask user for words
+  noun1 = input("Enter a noun: ")
+  adjective1 = input("Enter an adjective: ")
+  verb1 = input("Enter a verb: ")
+  noun2 = input("Enter a noun: ")
+  place1 = input("Enter a place: ")
+  adjective2 = input("Enter an adjective: ")
+  verb2 = input("Enter a verb: ")
 
   #Print the story with the user supplied words.
+  print("One day, I found a", noun1 + ".")
+  print("The", noun1, "was surprisingly", adjective1 + ".")
+  print("Suddenly, it began to", verb1 + ".")
+  print("I quickly grabbed my", noun2, "for help.")
+  print("Together, we ran to", place1 + ".")
+  print("Everyone there was shocked that it was", adjective2 + "!")
+  print("In the end, I decided to", verb2 + ".")
 
 
 
